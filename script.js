e4d93f753537964c76b7a8426544d1535ab54722
Yahir Ibarra <96273887+ReverendFreud@users.noreply.github.com>
2024-09-04 14:29:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Date updated
-document.getElementById('last-up').innerText = "Last update: Sep 3rd, 24; 1:51pm";
+document.getElementById('last-up').innerText = "Last update: Sep 4th, 24; 2:15pm";
 
 let demVotes = 0, repVotes = 0;
 let demColor = "black", repColor = "black";
@@ -16,17 +16,17 @@ var statePolls = {
 
     // Battleground states
     //state: calculateLead (Dem, Rep)
-    "AZ": calculateLead(47.9, 48.4), // Arizona
-    "FL": calculateLead(44.5, 49.0), // Florida
+    "AZ": calculateLead(47.5, 48.5), // Arizona
+    "FL": calculateLead(44.6, 49.0), // Florida
     "GA": calculateLead(48.3, 47.3), // Georgia
-    "MI": calculateLead(48.1, 46.2), // Michigan
+    "MI": calculateLead(47.7, 45.8), // Michigan
     "MN": calculateLead(49.8, 42.2), // Minnesota
-    "NV": calculateLead(48.5, 47.7), // Nevada
-    "NH": calculateLead(51.7, 44.8), // New Hampshire
+    "NV": calculateLead(48.5, 47.6), // Nevada
+    "NH": calculateLead(51.8, 44.7), // New Hampshire
     "NC": calculateLead(48.0, 48.4), // North Carolina
-    "PA": calculateLead(48.0, 46.9), // Pennsylvania
+    "PA": calculateLead(47.9, 46.9), // Pennsylvania
     "VA": calculateLead(48.2, 42.6), // Virginia
-    "WI": calculateLead(49.4, 46.2), // Wisconsin
+    "WI": calculateLead(49.4, 45.8), // Wisconsin
     //meh states
     "AL": calculateLead(38,57), // Alabama
     "AK": calculateLead(36,55), // Alaska
@@ -48,28 +48,28 @@ var statePolls = {
     "MA": calculateLead(47, 29), // Massachusetts
     "MS": calculateLead(37, 49), // Mississippi
     "MO": calculateLead(41, 59), // Missouri
-    "MT": calculateLead(38.2, 56.9), // Montana
+    "MT": calculateLead(38.2, 56.8), // Montana
     "NE": calculateLead(42, 50), // Nebraska
     "NJ": calculateLead(41, 36), // New Jersey
     "NM": calculateLead(51, 40), // New Mexico
     "NY": calculateLead(58, 42), // New York
     "ND": calculateLead(28, 62), // North Dakota
-    "OH": calculateLead(43.2, 50), // Ohio
+    "OH": calculateLead(43.2, 50.4), // Ohio
     "OK": calculateLead(30, 61), // Oklahoma
     "OR": calculateLead(45, 40), // Oregon
     "RI": calculateLead(40, 33), // Rhode Island
     "SC": calculateLead(37, 51), // South Carolina
     "SD": calculateLead(37, 51), // South Dakota
     "TN": calculateLead(28, 58), // Tennessee
-    "TX": calculateLead(45.6, 51.9), // Texas
+    "TX": calculateLead(44.8, 49.7), // Texas
     "UT": calculateLead(28, 60), // Utah
     "VT": calculateLead(70, 29), // Vermont
-    "WA": calculateLead(52.6,37.5), // Washington
+    "WA": calculateLead(52.7, 37.5), // Washington
     "WV": calculateLead(28, 55), // West Virginia
     "WY": calculateLead(15, 68), // Wyoming
     "DC": calculateLead(85, 5.8),  // Washington, D.C.
     "NE1": calculateLead(1, 99), 
-    "NE2": calculateLead(48.8, 43.0), 
+    "NE2": calculateLead(48.8, 43.1), 
     "NE3": calculateLead( 1 ,99), 
     "ME1": calculateLead(1.1, 98.9), 
     "ME2": calculateLead(71, 29) 
@@ -184,15 +184,10 @@ function calculateLead(dem, rep)
 
 
 //DISPLAY INFO
-
-//states array
 const hoverSquare = document.getElementById('hover-square');
 
 //SVG ID = map
 const svgMap = document.getElementById('map');
-
-// Cache the bounding rectangle of the SVG ID = map
-const svgRect = svgMap.getBoundingClientRect();
 
 svgMap.addEventListener('mouseover', function (e) 
 {
@@ -224,13 +219,9 @@ svgMap.addEventListener('mouseover', function (e)
             `<br><br>` +
         `</div>`;
 
-        // Get mouse position relative to the SVG container
-        const mouseX = e.clientX - svgRect.left + 360;
-        const mouseY = e.clientY - svgRect.top;
-
         // Position the square and make it visible
-        hoverSquare.style.left = mouseX + 'px';
-        hoverSquare.style.top = mouseY + 'px';
+        hoverSquare.style.left = e.clientX + 'px';
+        hoverSquare.style.top = e.clientY + 'px';
         hoverSquare.style.display = 'block';
 
     }//end if
@@ -245,9 +236,6 @@ svgMap.addEventListener('mouseout', function (e)
 
 //SVG ID = battleground-states
 const svgBgMap = document.getElementById('map-battleground');
-
-// Cache the bounding rectangle of the SVG ID = battleground-states
-const svgRectBg = svgBgMap.getBoundingClientRect();
 
 svgBgMap.addEventListener('mouseover', function (e) 
 {
@@ -280,13 +268,9 @@ svgBgMap.addEventListener('mouseover', function (e)
             `<br><br>` +
         `</div>`;
 
-        // Get mouse position relative to the SVG container
-        const mouseX = e.clientX - svgRectBg.left;
-        const mouseY = e.clientY - svgRectBg.top;
-
         // Position the square and make it visible
-        hoverSquare.style.left = mouseX + 'px';
-        hoverSquare.style.top = mouseY + 'px';
+        hoverSquare.style.left = e.clientX + 'px';
+        hoverSquare.style.top = e.clientY + 'px';
         hoverSquare.style.display = 'block';
 
     }//end if
