104f234f390f5d0c04586d87d7ac99ca3cf398bf
leos-codes <yahir.leo.ibarra@gmail.com>
2024-10-03 11:19:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Date updated
-document.getElementById('last-up').innerText = "Last update: Oct 2nd, 24; 5:38pm";
+document.getElementById('last-up').innerText = "Last update: Oct 3rd, 24; 11:19am";
 
 let demVotes = 0, repVotes = 0;
 let demColor = "black", repColor = "black";
@@ -16,17 +16,17 @@ var statePolls = {
 
     // Battleground states
     //state: calculateLead (Dem, Rep)
-    "AZ": calculateLead(47.5, 48.7), // Arizona
-    "FL": calculateLead(46.1, 49.7), // Florida
-    "GA": calculateLead(47.8, 48.8), // Georgia
-    "MI": calculateLead(48.8, 46.7), // Michigan
-    "MN": calculateLead(49.9, 43.9), // Minnesota
-    "NV": calculateLead(49, 47.1), // Nevada
-    "NH": calculateLead(51.9, 44.2), // New Hampshire
+    "AZ": calculateLead(47.5, 48.6), // Arizona
+    "FL": calculateLead(46.4, 49.7), // Florida
+    "GA": calculateLead(47.8, 48.6), // Georgia
+    "MI": calculateLead(48.8, 46.8), // Michigan
+    "MN": calculateLead(49.9, 44), // Minnesota
+    "NV": calculateLead(49, 47.2), // Nevada
+    "NH": calculateLead(51.9, 44.3), // New Hampshire
     "NC": calculateLead(48, 48.4), // North Carolina
     "PA": calculateLead(48.8, 47.5), // Pennsylvania
-    "VA": calculateLead(51, 43.7), // Virginia
-    "WI": calculateLead(49.4, 47.3), // Wisconsin
+    "VA": calculateLead(50.6, 43.0), // Virginia
+    "WI": calculateLead(49.4, 47.4), // Wisconsin
     //meh states
     "AL": calculateLead(38,57), // Alabama
     "AK": calculateLead(36,55), // Alaska
@@ -38,7 +38,7 @@ var statePolls = {
     "HI": calculateLead(42, 34), // Hawaii
     "ID": calculateLead(26, 55), // Idaho
     "IL": calculateLead(43, 34), // Illinois
-    "IN": calculateLead(40.1, 56.1), // Indiana
+    "IN": calculateLead(40.1, 56.2), // Indiana
     "IA": calculateLead(39, 51), // Iowa
     "KS": calculateLead(31, 47), // Kansas
     "KY": calculateLead(26, 55), // Kentucky
@@ -48,23 +48,23 @@ var statePolls = {
     "MA": calculateLead(47, 29), // Massachusetts
     "MS": calculateLead(37, 49), // Mississippi
     "MO": calculateLead(42.7, 54.1), // Missouri
-    "MT": calculateLead(40, 56.6), // Montana
-    "NE": calculateLead(42, 50), // Nebraska
+    "MT": calculateLead(40, 56.7), // Montana
+    "NE": calculateLead(39.8, 54.8), // Nebraska
     "NJ": calculateLead(41, 36), // New Jersey
-    "NM": calculateLead(50.5, 41.9), // New Mexico
+    "NM": calculateLead(50.5, 42), // New Mexico
     "NY": calculateLead(58, 42), // New York
     "ND": calculateLead(28, 62), // North Dakota
     "OH": calculateLead(43.8, 52.1), // Ohio
     "OK": calculateLead(30, 61), // Oklahoma
     "OR": calculateLead(45, 40), // Oregon
-    "RI": calculateLead(54.4, 39.2), // Rhode Island
+    "RI": calculateLead(54.4, 39.3), // Rhode Island
     "SC": calculateLead(37, 51), // South Carolina
     "SD": calculateLead(37, 51), // South Dakota
     "TN": calculateLead(28, 58), // Tennessee
-    "TX": calculateLead(45.5, 51.4), // Texas
+    "TX": calculateLead(45.6, 51.5), // Texas
     "UT": calculateLead(28, 60), // Utah
     "VT": calculateLead(70, 29), // Vermont
-    "WA": calculateLead(53.7, 35.9), // Washington
+    "WA": calculateLead(53.7, 36), // Washington
     "WV": calculateLead(28, 55), // West Virginia
     "WY": calculateLead(15, 68), // Wyoming
     "DC": calculateLead(85, 5.8),  // Washington, D.C.
